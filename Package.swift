f9d395363afd4747a594c8b82ebb722930c54afe
lion-33 <73334315+lion-33@users.noreply.github.com>
2026-05-08 21:26:51 -0500

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -4,15 +4,15 @@
 import PackageDescription
 
 let package = Package(
-    name: "YouTubeKit",
+    name: "MyTubeKit",
     /*platforms: [
         .macOS(.v12), .iOS(.v15), .watchOS(.v8), .tvOS(.v15)
     ],*/
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
-            name: "YouTubeKit",
-            targets: ["YouTubeKit"]),
+            name: "MyTubeKit",
+            targets: ["MyTubeKit"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
@@ -22,11 +22,11 @@ let package = Package(
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
-            name: "YouTubeKit",
+            name: "MyTubeKit",
             dependencies: [],
             resources: [.process("Resources")]),
         .testTarget(
-            name: "YouTubeKitTests",
-            dependencies: ["YouTubeKit"]),
+            name: "MyTubeKitTests",
+            dependencies: ["MyTubeKit"]),
     ]
 )
